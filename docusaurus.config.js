df5ae5383628a04c0accbaddca6cb6ffe2cfd89c
TechyGiraffe999 <92249532+TecEash1@users.noreply.github.com>
2024-02-25 15:04:19 +0000

    Fix again

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
   projectName: 'movieweb-backendocs', // Usually your repo name.
   deploymentBranch: 'main',
   trailingSlash: false,
-  
+
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
@@ -47,7 +47,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/TecEash1/movieweb-backendocs',
         },
         blog: false,
         theme: {
